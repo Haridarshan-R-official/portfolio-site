6d0c5e288631140e0295dc239819970864a91c76
Haridarshan-R-official <haridarshan.r.official@gmail.com>
2025-07-25 22:31:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function openModal(id) {
   document.getElementById(`modal-${id}`).style.display = 'flex';
 }
+
 function closeModal(id) {
   document.getElementById(`modal-${id}`).style.display = 'none';
-}
-
-document.getElementById('theme-toggle').addEventListener('click', () => {
-  document.body.classList.toggle('light-mode');
-});
+} 
