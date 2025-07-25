4136b0cb9d77e62289b8762e51c9575a4cb642d2
Haridarshan-R-official <haridarshan.r.official@gmail.com>
2025-07-25 22:43:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ function openModal(id) {
 
 function closeModal(id) {
   document.getElementById(`modal-${id}`).style.display = 'none';
-} 
+}
+
+function triggerFill(el) {
+  const bar = el.querySelector('.fill-bar');
+  bar.style.width = '100%';
+  setTimeout(() => {
+    openModal(bar.closest('.project-card').onclick.name);
+  }, 1000);
+}
